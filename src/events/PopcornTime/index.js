61f7fc211ed575a2bb37c835802537c2434eeaaa
Pranay Prakash <pranay.gp@gmail.com>
2017-04-27 23:27:56 -0500

    my last comment was mis-messaged

--- src/events/PopcornTime/index.js
+++ src/events/PopcornTime/index.js
@@ -7,8 +7,7 @@ const PopcornTime = require('../../sources/PopcornTime')
 // const INTERVAL: number = 1000 * 60 * 60 // 1 hour TODO: Move to config
 const INTERVAL: number = 1000 * 5 // 5 seconds
 
-// every INTERVAL seconds, check the source for inputs, diff the incoming inputs with a stored previous inputs array, update that array and then only emit events that had atleast 1 "diffed" input
-// let lastCheckedEpisodes = [];
+// every INTERVAL seconds, check the source for inputs, diff the incoming inputs with the last received inputs and then only emit events that had atleast 1 "diffed" input
 const source$ = Rx.Observable
       .interval(INTERVAL)
       .exhaustMap(() => PopcornTime({ shows: ['tt4179452', 'tt3530232', 'tt2575988'] })) // TODO: Don't hardcode. Move to config, and then to input
