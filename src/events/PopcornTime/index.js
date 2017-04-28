169bc4a2f80c1fb84a23262b05f6a156f5655e8b
Pranay Prakash <pranay.gp@gmail.com>
2017-04-27 23:26:25 -0500

    Switch to paorwise method for PopcornTime

--- src/events/PopcornTime/index.js
+++ src/events/PopcornTime/index.js
@@ -4,21 +4,20 @@ const _ = require('lodash')
 
 const PopcornTime = require('../../sources/PopcornTime')
 
-const INTERVAL: number = 1000 * 60 * 60 // 1 hour
-// const INTERVAL: number = 1000 * 5 // 5 seconds
+// const INTERVAL: number = 1000 * 60 * 60 // 1 hour TODO: Move to config
+const INTERVAL: number = 1000 * 5 // 5 seconds
 
 // every INTERVAL seconds, check the source for inputs, diff the incoming inputs with a stored previous inputs array, update that array and then only emit events that had atleast 1 "diffed" input
-let lastCheckedEpisodes = [];
-const source = Rx.Observable
+// let lastCheckedEpisodes = [];
+const source$ = Rx.Observable
       .interval(INTERVAL)
-      .exhaustMap(() => PopcornTime({ shows: ['tt4179452', 'tt3530232', 'tt2575988'] }))
-      .map(episodes => _.differenceBy(episodes, lastCheckedEpisodes, 'tvdb_id'))
-      .do(episodes => lastCheckedEpisodes = lastCheckedEpisodes.concat(episodes))
+      .exhaustMap(() => PopcornTime({ shows: ['tt4179452', 'tt3530232', 'tt2575988'] })) // TODO: Don't hardcode. Move to config, and then to input
+      .pairwise()
+      .map(([a, b]) => _.differenceBy(b, a, 'tvdb_id'))
       .filter(episodes => episodes.length)
-      .skip(1)              // comment if you want the initial list of events 
 
-// source
+// source$
 //   .map(episodes => episodes.map(e => e.title))
 //   .subscribe(console.log)
 
-module.exports = source
+module.exports = source$
