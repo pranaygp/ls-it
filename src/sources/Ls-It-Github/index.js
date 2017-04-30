6f2a4eefd8f09cb583a05d0e2465bfd1307724be
Pranay Prakash <pranay.gp@gmail.com>
2017-04-30 00:48:25 -0500

    Ls-It only for issues

--- src/sources/Ls-It-Github/index.js
+++ src/sources/Ls-It-Github/index.js
@@ -5,6 +5,7 @@ const LsItGithub$ = Webhooks$
   .pluck('payload')
   .filter(x => x.alias === "ls-it-github")
   .pluck('data')
+  .filter(data => data.issue)
   .map(event.bind({}, "ls-it-github"))
 
 module.exports = LsItGithub$
