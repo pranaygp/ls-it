c5bdb309c9d7b2135546c2ed62df6bb83c36ee73
Pranay Prakash <pranay.gp@gmail.com>
2017-04-30 22:48:25 -0500

    Setup HTTPS server if config files provided

--- src/sources/Webhooks/server.js
+++ src/sources/Webhooks/server.js
@@ -1,13 +1,17 @@
 const http = require('http');
+const crypto = require('crypto')
+const fs = require('fs')
+const config = require('../../../config.js')
 
+const server = http.createServer()
 
+if(config.cert){
+  const privateKey = fs.readFileSync(config.cert.key).toString();
+  const certificate = fs.readFileSync(config.cert.cert).toString();
 
-module.exports = http.createServer()
+  const credentials = crypto.createCredentials({key: privateKey, cert: certificate});
 
-// // getServer
-// module.exports = app => {
-//   if(server)
-//     return server
-//   server = https.createServer(app)
-//   return server
-// }
+  server.setSecure(credentials)
+}
+
+module.exports = server
