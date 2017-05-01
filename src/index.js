732495a742f06a5758c4fffdd4600c5c40f1f4df
Pranay Prakash <pranay.gp@gmail.com>
2017-04-30 20:14:14 -0500

    Add Websockets Broadcast

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const Rx = require('rx')
 const _ = require('lodash')
+const WebSocket = require('ws');
 const nodemailer = require('nodemailer');
 
 const reducer = require('./reducer')
 const Sources = require('./sources')
 
 const config = require('../config')
+const wss = new WebSocket.Server({ port: config.ws.port });
+console.log("Webscokets Broadcasting at port ", config.ws.port)
+
+
+// Monkeypatch broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -31,11 +44,20 @@ function sendMail(subject, text) {
   });
 }
 
+let currentList = []
 Rx.Observable
   .merge(_.map(Sources, _.identity))
   .startWith([])
-  .scan(reducer)
+  .scan(reducer)                  // gives us redux style state management
   .distinctUntilChanged()
+  .do(list => currentList = list) // HACK: side effect to store list for incoming client connections
   .subscribe(list => {
-    sendMail("TODO list", list.join('\n'))
+    // sendMail("TODO list", list.join('\n'))
+    wss.broadcast(JSON.stringify(list))
+  })
+
+  wss.on('connection', client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(currentList));
+    }
   })
