e7653c612c25ea90a4419c3afb8e0a5e62f98ba4
Pranay Prakash <pranay.gp@gmail.com>
2017-05-04 16:24:56 -0500

    always ping about issue or PR, regardless of creator

--- src/sources/Github/index.js
+++ src/sources/Github/index.js
@@ -5,7 +5,7 @@ const Github$ = Webhooks$
   .pluck('payload')
   .filter(x => x.alias === "github")
   .pluck('data')
-  .filter(data => (data.issue && data.issue.user.login !== "pranaygp") || (data.pull_request && data.pull_request.user.login !== "pranaygp"))
+  .filter(data => data.issue || data.pull_request)
   .map(event.bind({}, "github"))
 
 module.exports = Github$
