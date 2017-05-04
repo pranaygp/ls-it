66ac53aee582dbac7e1772ef219b6633c304feed
Pranay Prakash <pranay.gp@gmail.com>
2017-05-04 16:22:29 -0500

    remove in-imp github source

--- src/sources/index.js
+++ src/sources/index.js
@@ -3,6 +3,5 @@ module.exports = {
   PopcornTime$: require('./PopcornTime'),
   Webhooks$: require('./Webhooks'),
   Github$: require('./Github'),
-  GithubMyAssignments$: require('./Github-My-Assignments'),
   ManualWebhook$: require('./ManualWebhook')
 }
